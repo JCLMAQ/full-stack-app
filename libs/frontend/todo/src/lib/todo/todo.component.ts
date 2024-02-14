a8f260bae0d574d72845a22127bbef13a8dec55f
JCM <jcl.maquinay@gmail.com>
2024-02-14 14:09:14 +0100

    Backend running

--- libs/frontend/todo/src/lib/todo/todo.component.ts
+++ libs/frontend/todo/src/lib/todo/todo.component.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TodoListComponent } from '../todo-list/todo-list.component';
 
 @Component({
   selector: 'full-stack-app-todo',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TodoListComponent],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
