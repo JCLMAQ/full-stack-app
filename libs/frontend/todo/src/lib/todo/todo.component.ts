cc6d581e96d3a913f320ab9a6dc3c372290637ea
JCM <jcl.maquinay@gmail.com>
2024-02-14 16:39:33 +0100

    Auto stash before rebase of "develop"

--- libs/frontend/todo/src/lib/todo/todo.component.ts
+++ libs/frontend/todo/src/lib/todo/todo.component.ts
@@ -1,13 +1,11 @@
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { TodoListComponent } from '../todo-list/todo-list.component';
 
 @Component({
   selector: 'full-stack-app-todo',
   standalone: true,
-  imports: [CommonModule, TodoListComponent],
+  imports: [CommonModule],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TodoComponent {}
