ce5bda9df86f5c8b8f925386fa6529f1a992d1b0
JCM <jcl.maquinay@gmail.com>
2024-04-07 17:15:13 +0200

    environment file

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,27 +1,22 @@
 import { Route } from '@angular/router';
 import { PageNotFoundComponent } from '@fe/pages';
-import { hasUnsavedChangesGuard } from '@fe/utilities';
 
 export const appRoutes: Route[] = [
   {
     path: 'tasks',
     loadChildren: () => import('@fe/task').then((m) => m.taskRoutes),
-    canDeactivate: [hasUnsavedChangesGuard]
   },
   {
     path: 'todos',
     loadChildren: () => import('@fe/todo').then((m) => m.todoRoutes),
-    canDeactivate: [hasUnsavedChangesGuard]
   },
   {
     path: 'posts',
     loadChildren: () => import('@fe/post').then((m) => m.postRoutes),
-    canDeactivate: [hasUnsavedChangesGuard]
   },
   {
     path: 'users',
     loadChildren: () => import('@fe/user').then((m) => m.userRoutes),
-    canDeactivate: [hasUnsavedChangesGuard]
   },
 
   {
