88085777b56e67de8a7c480db90303d9db4186ec
JCM <jcl.maquinay@gmail.com>
2024-04-20 16:24:48 +0200

    Cleaning

--- libs/frontend/ui/core/forms/src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './lib/+state/forms.actions';
-export * from './lib/+state/forms.interfaces';
-export * from './lib/+state/forms.reducer';
-export * from './lib/+state/forms.selectors';
-export { DynamicFormComponent } from './lib/dynamic-form/dynamic-form.component';
-export { ListErrorsComponent } from './lib/list-errors/list-errors.component';
-
-export * as ngrxFormsEffects from './lib/+state/forms.effects';
