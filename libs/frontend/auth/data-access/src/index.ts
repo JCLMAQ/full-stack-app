e137a79d8a1a7f1d5533e9ce420ad3d58411f104
JCM <jcl.maquinay@gmail.com>
2024-04-22 11:35:14 +0200

    Remove data-access

--- libs/frontend/auth/data-access/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/services/auth-guard';
-export * from './lib/services/local-storage-jwt.service';
-export * from './lib/services/token-interceptor.service';
-
-export { AuthStore } from './lib/auth.store';
