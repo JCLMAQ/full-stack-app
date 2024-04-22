17326f04a3c1d94391763eeacc3cdfab9033d09c
JCM <jcl.maquinay@gmail.com>
2024-04-22 13:26:33 +0200

    Cleaning core

--- libs/frontend/ui/core/error-handler/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/+state/error-handler.actions';
-export * from './lib/+state/error-handler.reducer';
-export * from './lib/error-handler-interceptor.service';
-
-export * as errorHandlerEffects from './lib/+state/error-handler.effects';
