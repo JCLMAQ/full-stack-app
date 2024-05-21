771d8380161205caa2d8f4a22acf445f17cb8649
JCM <jcl.maquinay@gmail.com>
2024-05-21 15:05:57 +0200

    debug script frontend start

--- scripts/run-env.js
+++ scripts/run-env.js
@@ -2,7 +2,7 @@
 const dotenv = require('dotenv');
 const child_process = require('child_process');
 const config = dotenv.config()
-const DEV_SERVER_PORT = process.env.API_PORT || 4200;
+const DEV_SERVER_PORT = process.env.API_FRONTEND_PORT || 4200;
 // const child = child_process.exec(`ng serve --port=${DEV_SERVER_PORT}`);
 const child = child_process.exec(`pnpm run configangular -- --environment=dev && nx serve frontend --proxy-config ./proxy.conf.json --port=${DEV_SERVER_PORT}`);
 
